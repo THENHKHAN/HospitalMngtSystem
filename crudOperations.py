086ce08900ba605c08f543c5eb43de8b6507f821
noorul <noorul.khan@zettalabs.com>
2023-12-02 11:46:49 +0530

    02dec2023 structuring functions

--- crudOperations.py
+++ crudOperations.py
@@ -5,68 +5,45 @@ from prettytable import PrettyTable
 
 class MyHospitalDB:
 
-    def deleteDoc(self, docTableName, userName):
-        try:
-            cursor, conn = getCursor()
-            # Using a parameterized query to prevent SQL injection
-            delQuery = f"DELETE FROM {docTableName} WHERE name = %s RETURNING *;"# By RETURNING * this : it will return deleted record.
-            # Execute the query with the parameter
-            cursor.execute(delQuery, (userName,))
-            '''
-            In this example, %s is a placeholder for the parameter in the DELETE query, and the actual value 
-            is provided as a tuple (userName,) in the execute method. This approach helps to prevent SQL injection.
-            '''
-            # Fetch the deleted record (if needed)
-            deleted_record = cursor.fetchone()
-            conn.commit()
-            print(f"You have deleted doctor : {deleted_record}")
-            print("Remaining Doctors are :",end=" ")
-            selectQuery = self.doctorDetails(docTableName)
+    def userData(self, username, password):
+        [cursor, conn] = getCursor()
+        checkUsername = f"SELECT name FROM user_details WHERE name = '{username}' "  # this will get all name. Here PW not required bcz there can with diff. username have same pw
+        cursor.execute(checkUsername)
+        existingUser = cursor.fetchone()  # it will get one
 
-        except psycopg2.Error as error:
-            print(f"Failed to DELETE data in table:{docTableName} ,", error)
+        if (existingUser):  # it will return true if username of same already exist in the user_details table
+            print("User already Exist")
+            # Additional logic for handling existing user
 
-        finally:
-            if conn:
-                conn.close()
-                cursor.close()
-                print("The Postgresql connection inside delete record is closed")
-        exit(1)
-
-    def addDoctor(self, docTableName, docCred):
-        # AllCOl: ['id', 'name', 'specialisation', 'age', 'address', 'contact', 'fee', 'monthly_salary']
-        try:
-            [name, spe, age, addr, cont, fees, mSalary] = docCred  # unpacking list of creds so that i can insert all in table
-            cursor, conn = getCursor()
-            query = f"SELECT * FROM {docTableName}" # doing to get all the columns name of table
+        else:  # '{username}' single quote is must while using any variable in ''' quote
+            query = f'''
+                        INSERT INTO user_details (name , password)
+                        VALUES ('{username}' , '{password}') 
+           '''
             cursor.execute(query)
-            docDet = cursor.fetchall()
-            column_names = [col[0] for col in cursor.description]  # In order to work this, you have to execute above two line.
-
-            print(f"All Columns in {docTableName} : {column_names}")
-            # Construct the INSERT query without single quotes around the table and column names
-            # insertDetsQuery = f'''
-            # INSERT INTO {docTableName} (name, specialisation, age, address, contact, fee, monthly_salary)
-            #         VALUES ('{name}', '{spe}', '{age}', '{addr}', '{cont}', '{fees}', '{mSalary}')
-            # '''
-            insertDetsQuery = f'''
-                    INSERT INTO doctor_details ({column_names[1]}, {column_names[2]}, {column_names[3]}, {column_names[4]}, {column_names[5]}, {column_names[6]}, {column_names[7]})
-                    VALUES ('{name}', '{spe}', '{age}', '{addr}', '{cont}', '{fees}', '{mSalary}')
-                '''
-
-            cursor.execute(insertDetsQuery)
             conn.commit()
-            print(f"Data INSERTED SUCCESSFULLY in  table:{docTableName} ")
+            cursor.close()
+            conn.close()
+            print("""
+                    ============================================
+                    !!Well Done!!Registration Done Successfully!!
+                    ============================================
+               """)
 
-        except psycopg2.Error as error:
-            print(f"Failed to INSERT data in table:{docTableName} ,", error)
-
-        finally:
-            if conn:
-                conn.close()
-                cursor.close()
-                print("The Postgresql connection is closed")
-                forContinue = input("press any key to continue!!")
+    def userLogin(self, userName, password):
+        [cursor, conn] = getCursor()
+        verifyUserNamePass = f"SELECT name FROM user_details WHERE name = '{userName}' and  password = '{password}'"  # username and with this password in this table must match both.
+        cursor.execute(verifyUserNamePass)
+        existingUserNamePass = cursor.fetchone()
+        flag = True
+        if existingUserNamePass:
+            print("User verified!!!")
+            conn.close()
+        else:
+            print("Username or password is WRONG :\U0001f612, Please try again!! ")
+            flag = False
+            conn.close()
+        return flag
 
     def doctorDetails(self, docTableName):
         try:
@@ -138,50 +115,79 @@ class MyHospitalDB:
                 cursor.close()
                 print("The Postgresql connection inside Show nurse details is closed")
 
+    def addDoctor(self, docTableName, docCred):
+        # AllCOl: ['id', 'name', 'specialisation', 'age', 'address', 'contact', 'fee', 'monthly_salary']
+        try:
+            [name, spe, age, addr, cont, fees,
+             mSalary] = docCred  # unpacking list of creds so that i can insert all in table
+            cursor, conn = getCursor()
+            query = f"SELECT * FROM {docTableName}"  # doing to get all the columns name of table
+            cursor.execute(query)
+            docDet = cursor.fetchall()
+            column_names = [col[0] for col in
+                            cursor.description]  # In order to work this, you have to execute above two line.
+
+            print(f"All Columns in {docTableName} : {column_names}")
+            # Construct the INSERT query without single quotes around the table and column names
+            # insertDetsQuery = f'''
+            # INSERT INTO {docTableName} (name, specialisation, age, address, contact, fee, monthly_salary)
+            #         VALUES ('{name}', '{spe}', '{age}', '{addr}', '{cont}', '{fees}', '{mSalary}')
+            # '''
+            insertDetsQuery = f'''
+                    INSERT INTO doctor_details ({column_names[1]}, {column_names[2]}, {column_names[3]}, {column_names[4]}, {column_names[5]}, {column_names[6]}, {column_names[7]})
+                    VALUES ('{name}', '{spe}', '{age}', '{addr}', '{cont}', '{fees}', '{mSalary}')
+                '''
+
+            cursor.execute(insertDetsQuery)
+            conn.commit()
+            print(f"Data INSERTED SUCCESSFULLY in  table:{docTableName} ")
+
+        except psycopg2.Error as error:
+            print(f"Failed to INSERT data in table:{docTableName} ,", error)
+
+        finally:
+            if conn:
+                conn.close()
+                cursor.close()
+                print("The Postgresql connection is closed")
+                forContinue = input("press any key to continue!!")
+
+    def deleteDoc(self, docTableName, userName):
+        try:
+            cursor, conn = getCursor()
+            # Using a parameterized query to prevent SQL injection
+            delQuery = f"DELETE FROM {docTableName} WHERE name = %s RETURNING *;"  # By RETURNING * this : it will return deleted record.
+            # Execute the query with the parameter
+            cursor.execute(delQuery, (userName,))
+            '''
+            In this example, %s is a placeholder for the parameter in the DELETE query, and the actual value 
+            is provided as a tuple (userName,) in the execute method. This approach helps to prevent SQL injection.
+            '''
+            # Fetch the deleted record (if needed)
+            deleted_record = cursor.fetchone()
+            conn.commit()
+            print(f"You have deleted doctor : {deleted_record}")
+            print("Remaining Doctors are :", end=" ")
+            selectQuery = self.doctorDetails(docTableName)
+
+        except psycopg2.Error as error:
+            print(f"Failed to DELETE data in table:{docTableName} ,", error)
+
+        finally:
+            if conn:
+                conn.close()
+                cursor.close()
+                print("The Postgresql connection inside delete record is closed")
+        exit(1)
+
+
     def showEnteredData(self, username, password):
         print(f"userName : {username}")
         print(f"password : {password}")
 
-    def userLogin(self, userName, password):
-        [cursor, conn] = getCursor()
-        verifyUserNamePass = f"SELECT name FROM user_details WHERE name = '{userName}' and  password = '{password}'"  # username and with this password in this table must match both.
-        cursor.execute(verifyUserNamePass)
-        existingUserNamePass = cursor.fetchone()
-        flag = True
-        if existingUserNamePass:
-            print("User verified!!!")
-            conn.close()
-        else:
-            print("Username or password is WRONG :\U0001f612, Please try again!! ")
-            flag = False
-            conn.close()
-        return flag
 
-    def userData(self, username, password):
-        [cursor, conn] = getCursor()
-        checkUsername = f"SELECT name FROM user_details WHERE name = '{username}' "  # this will get all name. Here PW not required bcz there can with diff. username have same pw
-        cursor.execute(checkUsername)
-        existingUser = cursor.fetchone()  # it will get one
 
-        if (existingUser):  # it will return true if username of same already exist in the user_details table
-            print("User already Exist")
-            # Additional logic for handling existing user
-
-        else:  # '{username}' single quote is must while using any variable in ''' quote
-            query = f'''
-                        INSERT INTO user_details (name , password)
-                        VALUES ('{username}' , '{password}') 
-           '''
-            cursor.execute(query)
-            conn.commit()
-            cursor.close()
-            conn.close()
-            print("""
-                    ============================================
-                    !!Well Done!!Registration Done Successfully!!
-                    ============================================
-               """)
-
+    # common show , add, delete and update functionality and then based on the user choice making for particular entity
     def showDetails(self, doc, nurse, patient, others):
         if doc == "doctorDet":
             docTableName = "doctor_details"
@@ -190,7 +196,8 @@ class MyHospitalDB:
             nurseTableName = "nurse_details"
             self.nurseDetails(nurseTableName)
 
-    def addDetails(self, doc="", nurse="", patient="", others=""):  # given default arg so that i can send from where this fun is getting a single arg according to the doc or nurse or other. and below in this function bodu i can verify what arg got passed in this function.
+    def addDetails(self, doc="", nurse="", patient="",
+                   others=""):  # given default arg so that i can send from where this fun is getting a single arg according to the doc or nurse or other. and below in this function bodu i can verify what arg got passed in this function.
         if doc == "doctor":
             # enter doctor details
             # ASKING THE DETAILS
@@ -211,15 +218,16 @@ class MyHospitalDB:
             self.addNurse(nurseTableName)
 
     def deleteDetails(self, doc="", nurse="", patient="", others=""):
-        if doc == "doctors" :
+        if doc == "doctors":
             docTableName = "doctor_details"
             print("Here are the Details of all Doctor: ")
             self.doctorDetails(docTableName)
-            print("Enter the Doctor's name of which you want to remove : ", end=" ") # id and username both are unique so we can anyone.username seems more intuitive
+            print("Enter the Doctor's name of which you want to remove : ",
+                  end=" ")  # id and username both are unique so we can anyone.username seems more intuitive
             d = input()
             self.deleteDoc(docTableName, d)
 
-        elif nurse == "nurse" :
+        elif nurse == "nurse":
             docTableName = "nurse_details"
             print("Here are the Details of all Nurses: ")
             self.nurseDetails(docTableName)
